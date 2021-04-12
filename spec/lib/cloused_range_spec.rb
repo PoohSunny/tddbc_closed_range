581b0752c32158956dd014cfa976f3cb1e3b361f
PoohSunny <poohsunny@r.recruit.co.jp>
2021-04-12 13:32:07 +0900

    名前が気になったのでリファクタ

--- spec/lib/cloused_range_spec.rb
+++ spec/lib/cloused_range_spec.rb
@@ -1,10 +1,10 @@
 describe ClosedRange do
-  shared_examples_for "lowerとupperを元に文字列表現を取得できる" do
+  shared_examples_for "文字列表現を取得" do
     subject { ClosedRange.new(lower: lower, upper: upper).to_s }
     it { is_expected.to eq expected }
   end
 
-  describe '下端点 3, 上端点 8' do
+  describe '下端点 3, 上端点 8の場合' do
     let(:lower) { 3 }
     let(:upper) { 8 }
     let(:expected) { '[3,8]' }

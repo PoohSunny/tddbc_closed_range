94e21077d3b6ffdf2c230f7c97c2c9d72446776a
PoohSunny <poohsunny@r.recruit.co.jp>
2021-04-12 13:30:48 +0900

    テストのリファクタリング。もう次を見据えてグルーピング

--- spec/lib/cloused_range_spec.rb
+++ spec/lib/cloused_range_spec.rb
@@ -1,6 +1,13 @@
 describe ClosedRange do
-  describe '下端点 3, 上端点 8 の整数閉区間の文字列表記は "[3,8]"' do
-    subject { ClosedRange.new(lower: 3, upper: 8).to_s }
-    it { should eq('[3,8]') }
+  shared_examples_for "lowerとupperを元に文字列表現を取得できる" do
+    subject { ClosedRange.new(lower: lower, upper: upper).to_s }
+    it { is_expected.to eq expected }
+  end
+
+  describe '下端点 3, 上端点 8' do
+    let(:lower) { 3 }
+    let(:upper) { 8 }
+    let(:expected) { '[3,8]' }
+    it_should_behave_like "lowerとupperを元に文字列表現を取得できる"
   end
 end
